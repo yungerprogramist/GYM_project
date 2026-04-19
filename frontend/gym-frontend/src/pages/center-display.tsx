05eaac656adecbef0f25b649828f981ac31a1dda
iamanonym <s.masalsky@yandex.ru>
2026-04-19 08:13:08 +0500

    #1 - Добавлена страница авторизации

--- frontend/gym-frontend/src/pages/center-display.tsx
+++ frontend/gym-frontend/src/pages/center-display.tsx
@@ -3,6 +3,7 @@ import './center-display.scss';
 import Header from './header/header';
 import LeftColumn from './left-column/left-column';
 import RightColumn from './right-column/right-column';
+import LoginPage from './login-page/login-page';
 
 const CenterDisplay = () => {
   return (
@@ -10,7 +11,7 @@ const CenterDisplay = () => {
       <Header />
       <div className="center-display-content">
         <LeftColumn />
-        <CenterColumn />
+        <CenterColumn component={LoginPage} componentProps={{}} />
         <RightColumn />
       </div>
     </main>
