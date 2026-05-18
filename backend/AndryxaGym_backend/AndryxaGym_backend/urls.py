547052c796fd83759756df8b6bf363edf11e54fb
yungerprogramist <alekseiviskovath228@mail.ru>
2026-05-18 09:32:41 +0500

    solution_errors

--- backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
+++ backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
@@ -3,22 +3,14 @@ URL configuration for AndryxaGym_backend project.
 """
 
 from django.contrib import admin
-from django.urls import path, include
-<<<<<<< HEAD
+from django.urls import path, include, static
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('notes.urls')),
     path('api/users/', include('users.urls')),
     path('api/users/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
-=======
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/exercises/', include('apps.exercises.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> feature/exercises
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
