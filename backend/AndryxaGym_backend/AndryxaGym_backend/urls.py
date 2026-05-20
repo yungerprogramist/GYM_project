8512125f76d77e793219dff3a89086cd98da6601
buffon228 <jojojojo.jojojo@yandex.ru>
2026-05-20 13:17:31 +0500

    feat: add workouts app

--- backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
+++ backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('notes.urls')),
     path('api/measurements/', include('apps.measurements.urls')),
+    path('api/workouts/', include('workouts.urls')),
     path('api/users/', include('users.urls')),
     path('api/users/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/exercises/', include('exercises.urls')),
