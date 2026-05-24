197215171c4568558d3cc749d97aae4e27300127
iamanonym <s.masalsky@yandex.ru>
2026-05-24 10:23:10 +0500

    Доработка получения даты из календаря

--- frontend/gym-frontend/src/pages/right-column/right-column.tsx
+++ frontend/gym-frontend/src/pages/right-column/right-column.tsx
@@ -1,12 +1,14 @@
 import './right-column.scss';
 import Calendar from '../../shared/ui/calendar/calendar';
+import { calendarDateStore } from '../../shared/ui/calendar/calendarDataStore';
 
 const RightColumn = ({ showCalendar = false }) => {
     if (showCalendar) {
+        const setSelectedDate = calendarDateStore((state: any) => state.setSelectedDate);
         return (<main className="right-column">
            <Calendar
             trainingDays={["2026-05-10", "2026-05-12", "2026-05-15"]}
-            onDaySelect={(d) => localStorage.setItem('selectedDate', d) }
+            onDaySelect={(d) => setSelectedDate(d) }
            />
         </main>);
     }
