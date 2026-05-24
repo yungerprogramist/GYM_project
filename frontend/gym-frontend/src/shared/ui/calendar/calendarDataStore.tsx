d2f65b6187bee7a44b7c72a6af6279a5568ff8ec
andrey20005 <z.andrey2005.10@gmail.com>
2026-05-24 14:01:31 +0500

    использование useDateStore

--- frontend/gym-frontend/src/shared/ui/calendar/calendarDataStore.tsx
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-export const calendarDateStore = create((set) => ({
-  selectedDate: new Date(),
-  setSelectedDate: (newDate: Date) => set({ selectedDate: newDate }),
-}));
