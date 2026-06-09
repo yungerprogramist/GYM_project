4db0fa48a9401b2f0fb72e15a53ba6abda5deade
yungerprogramist <alekseiviskovath228@mail.ru>
2026-06-09 20:10:24 +0500

    calendar_fix

--- backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
+++ backend/AndryxaGym_backend/AndryxaGym_backend/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf import settings
 from .admin_views import model_diagram
 
-from apps.calendar.views import (
+from calendar_custom.views import (
     CalendarView,
     StatisticsView,
     StatisticsPeriodView,
@@ -29,9 +29,9 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/users/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/exercises/', include('exercises.urls')),
-    path('api/calendar/month/<int:year>/<int:month>/', CalendarView.as_view()),
-    path('api/statistics/summary/', StatisticsView.as_view()),
-    path('api/statistics/period/', StatisticsPeriodView.as_view()),
-    path('api/exercises/recent/', RecentExerciseView.as_view()),
-    path('api/exercises/recent/update/', RecentExerciseUpdateView.as_view()),
+    path('api/v1/calendar/month/<int:year>/<int:month>/', CalendarView.as_view()),
+    path('api/v1/statistics/summary/', StatisticsView.as_view()),
+    path('api/v1/statistics/period/', StatisticsPeriodView.as_view()),
+    path('api/v1/exercises/recent/', RecentExerciseView.as_view()),
+    path('api/v1/exercises/recent/update/', RecentExerciseUpdateView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
