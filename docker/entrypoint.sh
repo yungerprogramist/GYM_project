ac59d135ba38f2899e5369d35367420c8c277299
retscu <retscu@yandex.com>
2026-06-18 18:29:22 +0500

    Add user + password

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,4 @@
-docker exec -it docker-backend-1 sh -c "cat > /entrypoint.sh << 'EOF'
 #!/bin/sh
 python AndryxaGym_backend/manage.py migrate
-python AndryxaGym_backend/manage.py createsuperuser --noinput || true
+python AndryxaGym_backend/manage.py createsuperuser --noinput --username admin --password admin --email admin@admin.com || true
 python AndryxaGym_backend/manage.py runserver 0.0.0.0:8000
-EOF"
-docker exec -it docker-backend-1 chmod +x /entrypoint.sh
-docker restart docker-backend-1
