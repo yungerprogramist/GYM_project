3afbfc36d1d23b61173e4f0da81752c03b80dce7
ssiinnffuull <4060036@gmail.com>
2026-06-19 14:30:47 +0500

    artefacts

--- backend/AndryxaGym_backend/exercises/tests.py
+++ backend/AndryxaGym_backend/exercises/tests.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import pytest
-
 from django.contrib.auth import get_user_model
-
 from rest_framework.test import APIClient
 from rest_framework import status
-
 from exercises.models import MuscleGroup, Exercise
-
 
 User = get_user_model()
 
@@ -49,7 +44,6 @@ def exercise(db, muscle_group):
 
 @pytest.mark.django_db
 class TestMuscleGroup:
-
     def test_muscle_group_created(self, muscle_group):
         assert muscle_group.name == 'Грудь'
 
@@ -59,7 +53,6 @@ class TestMuscleGroup:
 
 @pytest.mark.django_db
 class TestExercise:
-
     def test_exercise_created(self, exercise):
         assert exercise.name == 'Жим лёжа'
 
@@ -72,61 +65,31 @@ class TestExercise:
 
 @pytest.mark.django_db
 class TestExerciseAPI:
-
     def test_get_exercises_unauthorized(self, api_client):
         response = api_client.get('/api/v1/exercises/')
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-        assert response.status_code == status.HTTP_403_FORBIDDEN
-
-    def test_get_exercises_authorized(
-        self,
-        api_client,
-        user,
-        exercise
-    ):
+    def test_get_exercises_authorized(self, api_client, user, exercise):
         api_client.force_authenticate(user=user)
-
         response = api_client.get('/api/v1/exercises/')
-
         assert response.status_code == status.HTTP_200_OK
 
-    def test_admin_can_create_exercise(
-        self,
-        api_client,
-        admin_user,
-        muscle_group
-    ):
+    def test_admin_can_create_exercise(self, api_client, admin_user, muscle_group):
         api_client.force_authenticate(user=admin_user)
-
         data = {
             'name': 'Присед',
             'description': 'Упражнение на ноги',
             'muscle_group': muscle_group.id
         }
-
         response = api_client.post('/api/v1/exercises/', data)
-
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_regular_user_cannot_create_exercise(
-        self,
-        api_client,
-        user,
-        muscle_group
-    ):
+    def test_regular_user_cannot_create_exercise(self, api_client, user, muscle_group):
         api_client.force_authenticate(user=user)
-
         data = {
             'name': 'Становая тяга',
             'description': 'Упражнение на спину',
             'muscle_group': muscle_group.id
         }
-
         response = api_client.post('/api/v1/exercises/', data)
-
         assert response.status_code == status.HTTP_403_FORBIDDEN
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> origin/main
